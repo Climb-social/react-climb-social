526bd6a40583bf5a547b4a905f203430d8018f51
Paolo Memoli <paolo@100shapes.com>
2015-10-01 15:55:31 +0100

    updated interaction

--- src/components/Interaction.jsx
+++ src/components/Interaction.jsx
@@ -5,17 +5,23 @@ class Interaction extends React.Component {
 
     render() {
 
-            const twitter_id = this.props.link;
-
-            return(
-                <div>
-                    // <script type="text/javascript" async src="//platform.twitter.com/widgets.js"></script>
-                    <a href={'https://twitter.com/intent/tweet?in_reply_to=' + twitter_id }>Reply</a>
-                    <a href={'https://twitter.com/intent/retweet?tweet_id=' + twitter_id }>Retweet</a>
-                    <a href={'https://twitter.com/intent/favorite?tweet_id=' + twitter_id }>Favorite</a>
-                </div>
-            );
-        
+        {(() => {
+            if (this.props.body.source_type === 'twitter') {
+                const twitter_id = this.props.body.link.split('/')[5]; // TODO: Tidy up
+                return(
+                    <ul>
+                        <li><a href={'https://twitter.com/intent/tweet?in_reply_to=' + twitter_id }>Reply</a></li>
+                        <li><a href={'https://twitter.com/intent/retweet?tweet_id=' + twitter_id }>Retweet</a></li>
+                        <li><a href={'https://twitter.com/intent/favorite?tweet_id=' + twitter_id }>Favorite</a></li>
+                    </ul>
+                );
+            }
+            if (this.props.body.source_type === 'facebook') {
+                return(
+                    <ul></ul>
+                );
+            }
+        })();}
     }
 }
 
