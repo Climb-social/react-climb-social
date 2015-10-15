810c48fe2b21611ba4d8305c9473a79f77176345
Michele Memoli <michele@100shapes.com>
2015-10-15 19:20:04 +0100

    fix(message): removed pic.twitter urls
    Removed anchors to pic.twitter from cards

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -4,9 +4,13 @@ import React from 'react';
 class Message extends React.Component {
 
     render() {
+
+        const message = this.props.body
+            .replace(/<a href="http:\/\/pic.twitter.com\/[^>]*>([^<]+)<\/a>/g, '');
+
         return (
             <div className="climb__tile__message"
-                 dangerouslySetInnerHTML={{__html: this.props.body}}>
+                 dangerouslySetInnerHTML={{__html: message}}>
             </div>
         );
     }
