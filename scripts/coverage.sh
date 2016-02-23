9d8c9bb9f8230b37822666c00d77a0e16b2e90f7
Michele Memoli <michele@100shapes.com>
2016-02-23 21:45:19 +0000

    chore(Coverage): Fixed Coverage running
    Coverage was only running for files in tests/

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -3,4 +3,4 @@ set -e
 PATH=$(npm bin):$PATH
 
 rm -rf coverage
-./node_modules/.bin/istanbul cover ./node_modules/.bin/babel-tape-runner ./src/**/tests/*-test.js
+./node_modules/.bin/istanbul cover -x **/*-test.js ./node_modules/.bin/babel-tape-runner ./src/**/*-test.js
