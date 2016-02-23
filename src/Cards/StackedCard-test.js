b13217224dd579687658238965c5b511306816d8
Michele Memoli <michele@100shapes.com>
2016-02-23 22:18:25 +0000

    test(StackedCard): Added real props to test

--- src/Cards/StackedCard-test.js
+++ src/Cards/StackedCard-test.js
@@ -4,13 +4,54 @@ import TestUtils from 'react-addons-test-utils';
 
 import StackedCard from './StackedCard';
 
-test('StackedCard output classes', assert => {
+test('StackedCard output class', assert => {
   const renderer = TestUtils.createRenderer();
-  renderer.render(<StackedCard />);
+  const props = {
+    message: 'I am the message',
+    link: 'http://climb.social',
+    author: {
+      pic: 'http://url/to/pic.jpg',
+      name: 'ClimbSocialUK',
+      link: 'http://twitter.com/ClimbSocialUK',
+      username: 'ClimbSocialUK'
+    },
+    sourceType: 'MegaSocial'
+  };
+
+  renderer.render(<StackedCard {...props} />);
   const output = renderer.getRenderOutput();
 
-  const expected = 'Climb__Card Climb__Card--Stacked';
-  const actual = output.props.className;
-  assert.equal(actual, expected, 'StackedCard should have a `Climb__Card Climb__Card--Stacked` className');
-  assert.end();
+  assert.plan(3);
+  const expected = true;
+
+  let actual = output
+    .props.className
+    .includes('Climb__Card '); // Note: the Space is important here
+
+  assert.equal(
+    actual,
+    expected,
+    'StackedCard should have a `Climb__Card` className'
+  );
+
+  actual = output
+    .props.className
+    .includes('Climb__Card--Stacked');
+
+  assert.equal(
+    actual,
+    expected,
+    'StackedCard should have a `Climb__Card--Stacked` className'
+  );
+
+  actual = output
+    .props.className
+    .includes('Climb__Card--MegaSocial');
+
+  assert.equal(
+    actual,
+    expected,
+    'StackedCard should have a `Climb__Card--MegaSocial` className'
+  );
 });
+
--- src/Cards/StackedCard.js
+++ src/Cards/StackedCard.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
-const Card = () => {
+const StackedCard = ({
+  message,
+  link,
+  author: {
+    picture: pic,
+    name,
+    link: profileLink,
+    username
+  },
+  sourceType
+  }) => {
   return (
-    <div className='Climb__Card Climb__Card--Stacked'>
-      I'm the Stacked Card
+    <div className={ `Climb__Card Climb__Card--Stacked Climb__Card--${sourceType}` }>
+
+      <div className='Climb__Profile'>
+        <img src={ pic }
+             width='50'
+             height='50'
+             alt={`Profile picture of ${name}`} />
+        <h6>
+          { name }
+        </h6>
+
+        <h5>
+          <a href={ profileLink }>
+            @{ username }
+          </a>
+        </h5>
+      </div>
+
+      <div className='Climb__TextBody'>
+        <a href={ link }>
+          <p>{ message }</p>
+        </a>
+      </div>
+
+      <div className='Climb__Media'>
+        Image or video here
+      </div>
+
     </div>
   );
 };
 
-export default Card;
+StackedCard.propTypes = {
+  message: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  author: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired
+  }).isRequired,
+  sourceType: PropTypes.string.isRequired
+};
+
+export default StackedCard;
