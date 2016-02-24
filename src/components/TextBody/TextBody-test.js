eb03a8d53e412419e8cf63bd2cecf883ff146ec7
Michele Memoli <michele@100shapes.com>
2016-02-24 22:31:41 +0000

    chore(Build): Triggering Coverage reporting
    Runs by Travis CI

---
+++ src/components/TextBody/TextBody-test.js
@@ -0,0 +1,30 @@
+import test from 'tape';
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+
+import TextBody from './TextBody';
+
+test('TextBody output classes', assert => {
+  const renderer = TestUtils.createRenderer();
+  const props = {
+    text: `I'm <strong> HTML </strong>`
+  };
+
+  renderer.render(<TextBody {...props} />);
+  const output = renderer.getRenderOutput();
+
+  const expected = true;
+
+  let actual = output
+    .props.className
+    .includes('Climb__TextBody');
+
+  assert.equal(
+    actual,
+    expected,
+    'TextBody should have a `Climb__TextBody` className'
+  );
+
+  assert.end();
+});
+
