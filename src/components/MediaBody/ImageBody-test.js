d3e2dccde193e07ca61f718ce9a6787b4b9e3b60
Michele Memoli <michele@100shapes.com>
2016-02-27 18:34:16 +0000

    test(ImageBody): Test for correctly setting in inline style for the background image
    ImageBody sets an inline style for the passed-in url prop

--- src/components/MediaBody/ImageBody-test.js
+++ src/components/MediaBody/ImageBody-test.js
@@ -52,22 +52,52 @@ test('ImageBody styles: without height', assert => {
 });
 
 test('ImageBody styles: with height', assert => {
-  const height = 40;
+  assert.plan(2);
 
+  const height = 40;
+  const url = 'http://url.to.image.com';
   const props = {
-    url: 'http://url.to.image.com',
+    url,
     height
   };
 
   const output = shallow(<ImageBody {...props} />);
 
-  const expected = height;
-  const actual = output.prop('style').height;
+  let expected = height;
+  let actual = output.prop('style').height;
 
   assert.equal(
     expected,
     actual,
     'Outputs a height inline style'
+  );
+
+  expected = true;
+  actual = output.prop('style').hasOwnProperty('backgroundImage');
+
+  assert.equal(
+    expected,
+    actual,
+    'Outputs a backgroundImage inline style'
+  );
+});
+
+
+test('ImageBody inline image url', assert => {
+  const img = 'http://url.to.image.com';
+  const props = {
+    url: img
+  };
+
+  const output = shallow(<ImageBody {...props} />);
+
+  const expected = `url(${img})`;
+  const actual = output.prop('style').backgroundImage;
+
+  assert.equal(
+    expected,
+    actual,
+    'backgroundImage url'
   );
 
   assert.end();
