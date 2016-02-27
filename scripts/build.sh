9e7a9ada5b890a6c66d5ac3a90267ff9bb682021
Michele Memoli <michele@100shapes.com>
2016-02-27 22:50:02 +0000

    chore(Build): Added NODE_ENV=production flag and removed tests from compile
    Told babel to ignore all test scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,9 @@
 
 # build minified standalone version in dist
 rm -rf dist
-./node_modules/.bin/webpack --output-filename=dist/react-climb-social.js
-./node_modules/.bin/webpack --output-filename=dist/react-climb-social.min.js --optimize-minimize
+NODE_ENV=production ./node_modules/.bin/webpack --output-filename=dist/react-climb-social.js
+NODE_ENV=production ./node_modules/.bin/webpack --output-filename=dist/react-climb-social.min.js --optimize-minimize
 
 # build ES5 modules to lib
 rm -rf lib
-./node_modules/.bin/babel src --out-dir lib
+./node_modules/.bin/babel src --out-dir lib --ignore="**/*-test.js"
