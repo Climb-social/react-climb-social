c325e1605aa5ac5b9d9f8cdf5cc489f1d7a6609f
Michele Memoli <michele@100shapes.com>
2016-02-27 21:40:41 +0000

    feat(Climb): Added ability to render Climb components into DOM elements with .Climb classes.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-export { default as ClimbView } from './views/ClimbView/ClimbView';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ClimbView from './views/ClimbView/ClimbView';
+export { ClimbView as ClimbView };
 export { default as ListView } from './views/ListView/ListView';
 export { default as ColumnView } from './views/ColumnView/ColumnView';
 export { default as RegularSquareView} from './views/RegularSquareView/RegularSquareView';
@@ -10,3 +14,27 @@ export { default as VideoBody } from './components/MediaBody/VideoBody';
 export { default as TextBody } from './components/TextBody/TextBody';
 export { default as Timestamp } from './components/Timestamp/Timestamp';
 export { default as Publisher } from './components/Publisher/Publisher';
+
+const renderClimbDisplays = () => {
+  const ClimbViews = document.querySelectorAll('.Climb');
+  Array.from(ClimbViews).forEach(elem => {
+    const collectionId = elem.dataset.collectionId;
+
+    ReactDOM.render(
+      React.createElement(ClimbView, { collectionId }),
+      elem
+    );
+  });
+};
+
+const isBrowser = () => {
+  try {
+    return this === window;
+  } catch (e) {
+    return false;
+  }
+};
+
+if (isBrowser) {
+  document.addEventListener('DOMContentLoaded', renderClimbDisplays);
+}
