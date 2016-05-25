6f3201d3e9ba7ffd8bac6e0f3b0be6e7ff49cacc
James Greenaway <james@greenaway.io>
2016-05-25 17:50:00 +0100

    fix(ColumnLayout): Fix image layout, fix styleNames

--- src/components/layouts/ColumnLayout/ColumnLayout.js
+++ src/components/layouts/ColumnLayout/ColumnLayout.js
@@ -1,12 +1,31 @@
 import React, { PropTypes } from 'react';
-import css from 'react-css-modules';
 import bricks from 'bricks.js';
 
-import StackedCard from '../../cards/StackedCard/StackedCard';
 import styles from './ColumnLayout.sass';
+import StackedCard from '../../cards/StackedCard/StackedCard';
 
 
-class ColumnLayout extends React.Component {
+export default class ColumnLayout extends React.Component {
+
+  static propTypes = {
+    Card: PropTypes.oneOfType([PropTypes.func, PropTypes.elem]).isRequired,
+    items: PropTypes.array,
+    sizes: PropTypes.array,
+  };
+
+  static defaultProps = {
+    Card: StackedCard,
+    items: [],
+    sizes: [
+      { columns: 1, gutter: 30 },
+      { mq: '640px', columns: 2, gutter: 30 },
+      { mq: '945px', columns: 3, gutter: 30 },
+      { mq: '1250px', columns: 4, gutter: 30 },
+      { mq: '1556px', columns: 5, gutter: 30 },
+      { mq: '1860px', columns: 6, gutter: 30 },
+    ],
+  };
+
 
   componentDidMount() {
     this.packer = bricks({
@@ -30,37 +49,21 @@ class ColumnLayout extends React.Component {
   render() {
     const { Card, items } = this.props;
 
+    // Use className in this component due to props bug in react-css-modules
+    // https://github.com/gajus/react-css-modules/issues/58
     return (
-      <div className="Climb--ColumnLayout" styleName="root">
-        <div className="Climb--ColumnLayout--inner">
+      <div className="Climb--ColumnLayout" className={`Climb--ColumnLayout ${styles.root}`}>
+        <div className="Climb--ColumnLayout__inner">
           {items.map(item =>
-            <div key={item.id} style={{ width: 290 }} styleName="card">
-              <Card {...item} />
-            </div>
+            <div
+              key={item.id}
+              style={{ width: 290 }}
+              className={`Climb--ColumnLayout__item ${styles.item}`}
+              children={<Card {...item} />}
+            />
           )}
         </div>
       </div>
     );
   }
 }
-
-ColumnLayout.propTypes = {
-  Card: PropTypes.oneOfType([PropTypes.func, PropTypes.elem]).isRequired,
-  items: PropTypes.array,
-  sizes: PropTypes.array,
-};
-
-ColumnLayout.defaultProps = {
-  Card: StackedCard,
-  items: [],
-  sizes: [
-    { columns: 1, gutter: 30 },
-    { mq: '640px', columns: 2, gutter: 30 },
-    { mq: '945px', columns: 3, gutter: 30 },
-    { mq: '1250px', columns: 4, gutter: 30 },
-    { mq: '1556px', columns: 5, gutter: 30 },
-    { mq: '1860px', columns: 6, gutter: 30 },
-  ],
-};
-
-export default css(ColumnLayout, styles);
